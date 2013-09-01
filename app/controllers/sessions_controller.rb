9b0df94f8689b924bd74a26772de8d4e73242d12
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-01 11:36:19 -0700

    make Sessions#destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,11 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    session[:poet_id] = nil
+    redirect_to root_path
+  end
+
   protected
 
   def auth
