a1ba93c1078439283aae417d5999d85a77048b0a
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-01 10:06:41 -0700

    modify view to have basic sign in, sign out links and add current user method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def check_logged_in!
+    redirect_to root_url, { flash: { error: 'You must be signed in to do that!' } } unless current_user
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
     @poet = Poet.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Poet.create_with_omniauth(auth)
-    self.current_user = @poet
-    redirect_to '/'
+    session[:poet_id] = poet.id
+    redirect_to root_path
   end
 
   protected
