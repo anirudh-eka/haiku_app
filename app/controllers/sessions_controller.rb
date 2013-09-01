1bc38b9ffcc68d230181196c84c6100ef8fb05a4
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-01 13:13:17 -0700

    getting twitter user instantiation to work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    puts "*"* 100
-    auth[:credentials]
     poet = Poet.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Poet.create_with_omniauth(auth)
     session[:poet_id] = poet.id
+
+    poet = Twitter::Client.new(
+      oauth_token: auth[:credentials][:token],
+      oauth_token_secret: auth[:credentials][:secret]
+    )
+    Thread.new{poet.update("Tweeting Poet")}
+
     redirect_to root_path
   end
 
