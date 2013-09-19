e1e1d9749b99f6c445b9b11b0ed1fba87bd4d087
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-19 14:52:55 -0700

    add poets#index action, modify poets route to exclude create

--- app/controllers/poets_controller.rb
+++ app/controllers/poets_controller.rb
@@ -1,6 +1,10 @@
 class PoetsController < ApplicationController
   skip_before_action :require_login, only: [:new]
 
+  def index
+    @poets = Poet.all
+  end
+
   def new
     redirect_to root_path
   end
--- spec/controllers/poets_controller_spec.rb
+++ spec/controllers/poets_controller_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
 
 describe PoetsController do
+  
+  let(:new_poet) { FactoryGirl.create(:poet) }      
+  
   describe 'GET #index' do
+    context "if user is not signed in" do
+      it "should redirect to new_poet_path" do
+        get :index
+        expect(response).to redirect_to new_poet_path
+      end
+    end
+    
+    context "if user is signed in " do
+      before(:each) do
+        session[:poet_id] = new_poet.id
+      end
+
+      it "should return an array of all poets" do
+        poets_array = [new_poet]
+        5.times{poets_array << FactoryGirl.create(:poet)}
+
+        get :index
+        assigns(:poets).should eq(poets_array)
+      end
+    end
+  end
+
+  describe 'GET #new' do
     it "should redirect to root_path" do
       get :new
       expect(response).to redirect_to root_path
