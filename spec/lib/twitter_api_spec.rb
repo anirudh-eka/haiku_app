dd31f338819d2c0f3f466f816cb80328224af07b
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-19 13:15:44 -0700

    finish twitter api test

--- spec/lib/twitter_api_spec.rb
+++ spec/lib/twitter_api_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe TwitterAPI do
-
+  let(:twitterapi) { TwitterAPI.new(1234567, 89191938) }
+  let(:content) { FactoryGirl.attributes_for(:poem)[:content] }
+  
   describe 'initialize' do
     it 'should create a new twitter client' do
-      twitterapi = TwitterAPI.new(1234567, 89191938)
       expect(twitterapi.client).to be_kind_of(Twitter::Client)
     end
   end
 
+  describe 'tweet' do
+    it 'should call update on client attribute passing content as argument' do
+      twitterapi.client.should_receive(:update).with(content)
+      twitterapi.tweet(content)
+    end
+  end
 end
