8eb6d51a6dbf5c73d10c18d1556d7c4bae5661b3
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-19 12:23:43 -0700

    finish initial PoetsController spec

--- app/controllers/poets_controller.rb
+++ app/controllers/poets_controller.rb
@@ -1,4 +1,7 @@
 class PoetsController < ApplicationController
+  skip_before_action :require_login, only: [:new]
+
   def new
+    redirect_to root_path
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,40 +1,38 @@
 require 'spec_helper'
 
 describe SessionsController do
+
   let(:new_poet) {FactoryGirl.create(:poet)}
   let(:omniauth_auth) {{"provider" => "twitter", "uid" => 123456789}}
   let(:request_env) {{'omniauth.auth' => :omniauth_auth}}
-  describe 'GET #create' do
-    context 'happy path' do
-      
+  describe 'GET #create' do      
     before(:each) do
       Poet.stub(:create_with_omniauth).and_return(new_poet)
       request.stub(:env).and_return(request_env)
     end
 
-      context 'when user logs in for the first time' do
-        it 'should call create_with_omniauth on the Poet object' do
-          Poet.should_receive(:create_with_omniauth).and_return(new_poet)#.with(auth)
-          get :create , :provider => "twitter"
-        end
+    context 'when user logs in for the first time' do
+      it 'should call create_with_omniauth on the Poet object' do
+        Poet.should_receive(:create_with_omniauth).and_return(new_poet)#.with(auth)
+        get :create , :provider => "twitter"
       end
-      context 'when user has logged in before' do
-        it 'should not call create_with_omniauth on the Poet object' do
-          Poet.stub(:find_by_provider_and_uid).and_return(new_poet)
-          Poet.should_not_receive(:create_with_omniauth)
-          get :create, :provider => "twitter"
-        end
-      end
-      
-      it 'should store poet id in session' do
-         get :create, :provider => "twitter"
-         expect(session[:poet_id]).to be(new_poet.id)
-      end
-
-      it 'should redirect to root path' do
+    end
+    context 'when user has logged in before' do
+      it 'should not call create_with_omniauth on the Poet object' do
+        Poet.stub(:find_by_provider_and_uid).and_return(new_poet)
+        Poet.should_not_receive(:create_with_omniauth)
         get :create, :provider => "twitter"
-        expect(response).to redirect_to root_path
       end
+    end
+    
+    it 'should store poet id in session' do
+       get :create, :provider => "twitter"
+       expect(session[:poet_id]).to be(new_poet.id)
+    end
+
+    it 'should redirect to root path' do
+      get :create, :provider => "twitter"
+      expect(response).to redirect_to root_path
     end
   end
 
