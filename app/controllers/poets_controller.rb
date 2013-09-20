9c35372653c8f071c97e02a9a45f61a180e36f29
Anirudh Dhullipalla <rudysback@gmail.com>
2013-09-19 19:18:00 -0700

    add index, show, and destroy actions to PoetsController and corresponding tests

--- app/controllers/poets_controller.rb
+++ app/controllers/poets_controller.rb
@@ -1,5 +1,5 @@
 class PoetsController < ApplicationController
-  skip_before_action :require_login, only: [:new]
+  skip_before_action :require_login, only: [:new, :show]
 
   def index
     @poets = Poet.all
@@ -7,5 +7,20 @@ class PoetsController < ApplicationController
 
   def new
     redirect_to root_path
+  end
+
+  def show
+    @poet = Poet.find(params[:id])
+    @poems = @poet.poems
+  end
+
+  def destroy
+    if params[:id].to_i == session[:poet_id].to_i
+      poet = Poet.find(params[:id])
+      poet.destroy
+      redirect_to root_path
+    else
+      redirect_to poet_path(id: params[:id])
+    end
   end
 end
