9a91489cfbec67932a9c5b41ec8065aa40dcdf86
Anirudh Dhullipalla <rudysback@gmail.com>
2013-10-07 19:14:08 +0530

    generate backbone files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,14 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require haiku_app
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
 
 $(document).foundation();
---
+++ app/assets/javascripts/haiku_app.js
@@ -0,0 +1,13 @@
+window.HaikuApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  HaikuApp.initialize();
+});
