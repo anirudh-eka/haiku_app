be9609dcab6c7c093982df8c7c254c52c2953b92
Anirudh Dhullipalla <rudysback@gmail.com>
2013-10-07 22:08:29 +0530

    change name of poems router file

--- app/assets/javascripts/routers/router.js
+++
@@ -1,9 +0,0 @@
-HaikuApp.Routers.Poems = Backbone.Router.extend({
-  routes: {
-    "" :"showHome" 
-  },
-
-  showHome: function() {
-    console.log('you are trying to reach home, you can do it! ET')
-  } 
-}); 
