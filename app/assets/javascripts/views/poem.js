cbeaa514a4e1c41e49279f3637ec5d053cf0b397
Anirudh Dhullipalla <rudysback@gmail.com>
2013-10-08 11:55:05 +0530

    remove poem index template because poem index is now a collection of individual poem views, rename poem.js to poem_show

--- app/assets/javascripts/views/poem.js
+++
@@ -1,8 +0,0 @@
-HaikuApp.Views.Poem = Backbone.View.extend({
-  tagName: 'article',
-
-  render: function() {
-    this.$el.html(JST['poems/show']({ poem: this.model.toJSON() }))
-    return this
-  }
-});
