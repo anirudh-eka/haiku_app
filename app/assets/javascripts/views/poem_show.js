5fe7b162b69429bee434b09970068c0cb9b28ef6
Anirudh Dhullipalla <rudysback@gmail.com>
2013-10-18 22:45:10 -0700

    make initial server side logic for clicking on snap button and changing snap_count

--- app/assets/javascripts/views/poem_show.js
+++ app/assets/javascripts/views/poem_show.js
@@ -1,17 +1,42 @@
 HaikuApp.Views.Poem = Backbone.View.extend({
   tagName: 'article',
   className: 'poem',
+  
+  events: {
+    'click .snap' : 'snap'
+  },
 
   render: function() {
-    var snapped = false
+    this.$el.html(this.renderPoem());
+    
     if (HaikuApp.currentUser) {
-      snapped = HaikuApp.currentUser.snapped(this.model.id)
+      this.$el.append(this.renderSnapButton());
     }
 
-    this.$el.html(JST['poems/show']({ poem: this.model.toJSON(), 
-      author: this.model.author(),
-      loggedIn: Boolean(HaikuApp.currentUser), 
-      snapped: snapped}))
     return this
+  },
+
+  renderPoem: function() {
+    return JST['poems/show']({ poem: this.model.toJSON(), author: this.model.author() })
+  },
+
+  renderSnapButton: function() {
+    var snapped = HaikuApp.currentUser.snapped(this.model.id)
+    return JST['snaps/button_show']({ snapped: snapped })
+  },
+
+  snap: function() {
+    var snapCount = this.model.get('snap_count')
+    console.log(snapCount)
+    snapCount += 1
+
+    this.model.save({snap_count: snapCount}, { 
+      success: function() {
+      console.log('success')
+      },
+      error: function() {
+        console.log('failure')
+      }
+    });
   }
 });
