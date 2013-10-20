b0a84cf0687fb6e6164d4ab8ee58f914d8563632
Anirudh Dhullipalla <rudysback@gmail.com>
2013-10-19 23:04:46 -0700

    make snap model validate for presence of poet_id and poem_id

--- app/models/snap.rb
+++ app/models/snap.rb
@@ -3,4 +3,6 @@ class Snap < ActiveRecord::Base
    belongs_to :poem
 
    validates_uniqueness_of :poem_id, :scope => [:poet_id]
+   validates_presence_of :poem_id
+   validates_presence_of :poet_id
 end
