3d9d75cedc40c076de37d538c5ecaba1c52aede7
Anirudh Dhullipalla <rudysback@gmail.com>
2013-10-23 12:19:16 -0700

    make validation in poem model that checks that snap_count is not negative

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,8 +1,12 @@
 class Poem < ActiveRecord::Base
-   belongs_to :poet
-   has_many :snaps
+  belongs_to :poet
+  has_many :snaps
 
-   validates_presence_of :content, message: "What is a poem without words?"
-   validates_length_of :content, maximum: 140, message: "Words are like light, enough can give you sight, too much can make you blind"
-
+  validates_presence_of :content, message: "What is a poem without words?"
+  validates_length_of :content, maximum: 140, message: "Words are like light, enough can give you sight, too much can make you blind"
+  validate :snap_count_cannot_be_negative
+  
+  def snap_count_cannot_be_negative
+    errors.add(:snap_count, "snap_count can't be negative") if snap_count < 0
+  end
 end
